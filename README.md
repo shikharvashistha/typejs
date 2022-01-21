15386fa99fbfca81c9b8f03c0a29f4f3a88d1a11
Shikhar Vashistha <51105234+shikharvashistha@users.noreply.github.com>
2022-01-21 05:49:07 +0000

    Added a redirect route

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## typejs
+
+Controllers are responsible for handling incoming requests and returning responses to the client.
+
+A controller's purpose is to receive specific requests for the application. The routing mechanism controls which controller receives which requests.
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+    @Get('docs')
+    @Redirect('https://github.com/shikharvashistha/typejs', 302)
+    getDocs() {
+        return { url: 'https://github.com/shikharvashistha/typejs' };
+    }
 }
